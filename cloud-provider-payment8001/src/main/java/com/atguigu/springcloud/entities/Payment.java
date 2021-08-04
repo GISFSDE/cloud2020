d1ae1691d0442756cfc05063cca42d156ec88c62
MrDemonlxl <714416426@qq.com>
2021-08-04 20:40:18 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+private Long id;
+private  String serial;
+}
